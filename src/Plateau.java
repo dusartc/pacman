22e1bc951b3a9961299fbbd2ea9fec3389674d6e
MoulardS <fappingmaterial@hotmail.fr>
2015-09-04 08:48:33 +0200

    oui

--- src/Plateau.java
+++ src/Plateau.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
-
 public class Plateau {
 
   private Cellule[][] plateau;
