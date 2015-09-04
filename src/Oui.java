bfc7d348fd6f9df5aa31a05ca9135f344d23599f
MoulardS <fappingmaterial@hotmail.fr>
2015-09-04 08:57:12 +0200

    Meilleurs tests

--- src/Oui.java
+++ src/Oui.java
@@ -6,46 +6,86 @@ import org.junit.Test;
 public class Oui {
 
   @Test
-  public void testCoordonnees() {
+  public void testCoordonneesX() {
+    Coordonnees coord = new Coordonnees(6,9);
+    assertEquals(coord.getX(), 6);
+  }
+  
+  @Test
+  public void testCoordonneesY() {
+    Coordonnees coord = new Coordonnees(6,9);
+    assertEquals(coord.getY(), 9);
+  }
+  
+  @Test
+  public void testCoordonneesComp() {
     Coordonnees coord = new Coordonnees(6,9);
     Coordonnees oui = new Coordonnees(6,9);
-    Coordonnees non = new Coordonnees(12,18);
-    assertEquals(coord.getX(), 6);
-    assertEquals(coord.getY(), 9);
     assertTrue(coord.compare(oui));
   }
   
   @Test
-  public void testCellule() {
+  public void testCelluleCoord() {
     Cellule c = new Cellule(6,9);
-    Piece p = new Piece();
-    Monstre m = new Monstre(1,1);
     Coordonnees coord = new Coordonnees(6,9);
     assertTrue(coord.compare(c.getCoordonnes()));
+  }
+  
+  @Test
+  public void testCelluleVide() {
+    Cellule c = new Cellule(6,9);
     assertTrue(c.isVide());
+  }
+  
+  @Test
+  public void testCellulePasVide() {
+    Cellule c = new Cellule(6,9);
+    Piece p = new Piece();
     c.setElement(p);
     assertFalse(c.isVide());
+  }
+  
+  @Test
+  public void testCellulePasMonstre() {
+    Cellule c = new Cellule(6,9);
     assertFalse(c.contientMonstre());
+  }
+  
+  @Test
+  public void testCelluleMonstre() {
+    Cellule c = new Cellule(6,9);
+    Monstre m = new Monstre(1,1);
     c.addMonstre(m);
     assertTrue(c.contientMonstre());
   }
 
+  
+  
   @Test
-  public void testEffet() {
+  public void testEffet0() {
+    
+    Effet e = new Effet(0);
+    assertFalse(e.isManger());
+    assertTrue(e.isRalentissement());
+    assertFalse(e.isPoints());
+
+  }
+  
+  @Test
+  public void testEffet1() {
     Effet e = new Effet(1);
     assertTrue(e.isManger());
     assertFalse(e.isRalentissement());
     assertFalse(e.isPoints());
-    
-    Effet e2 = new Effet(0);
-    assertFalse(e2.isManger());
-    assertTrue(e2.isRalentissement());
-    assertFalse(e2.isPoints());
-    
-    Effet e3 = new Effet(2);
-    assertFalse(e3.isManger());
-    assertFalse(e3.isRalentissement());
-    assertTrue(e3.isPoints());
+
+  }
+  
+  @Test
+  public void testEffet2() {
+    Effet e = new Effet(2);
+    assertFalse(e.isManger());
+    assertFalse(e.isRalentissement());
+    assertTrue(e.isPoints());
 
   }
   
